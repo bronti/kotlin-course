4563ba35bf396f88fd1b9d7075c42c1d64c2855d
Anya Yaveyn <anya.yaveyn@yandex.ru>
2017-11-05 23:30:44 +0300

    hw1 done

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,63 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+object PetrovskiyLanguage {
 
-    return words.joinToString(separator = " ")
+    enum class Sex {
+        FEMALE, MALE
+    }
+
+    enum class Part {
+        ADJECTIVE, NOUN, VERB
+    }
+
+    private val endingToSignature = hashMapOf(
+            "lios" to Pair(Part.ADJECTIVE, Sex.MALE),
+            "liala" to Pair(Part.ADJECTIVE, Sex.FEMALE),
+            "etr" to Pair(Part.NOUN, Sex.MALE),
+            "etra" to Pair(Part.NOUN, Sex.FEMALE),
+            "initis" to Pair(Part.VERB, Sex.MALE),
+            "inites" to Pair(Part.VERB, Sex.FEMALE)
+    )
+
+    private fun getEnding(str: String) = endingToSignature.keys.find { str.endsWith(it) }
+
+    fun getSignature(str: String): Pair<Part, Sex>? {
+        val ending = getEnding(str) ?: return null
+        return endingToSignature[ending]
+    }
+
+    private fun isValidWord(str: String) = getEnding(str) != null
+
+    fun isValidSentence(words: List<String>): Boolean {
+        if (words.isEmpty()) return false
+        if (words.size == 1) return isValidWord(words.single())
+
+        val (firstPart, firstSex) = getSignature(words[0]) ?: return false
+        if (firstPart == Part.VERB) return false
+
+        tailrec fun checkSentence(prevPart: Part, prevSex: Sex, tail: List<String>): Boolean {
+            if (tail.isEmpty()) return prevPart != Part.ADJECTIVE
+            val (part, sex) = getSignature(tail[0]) ?: return false
+            if (sex != prevSex
+                    || (prevPart == Part.ADJECTIVE && part == Part.VERB)
+                    || (prevPart != Part.ADJECTIVE && part != Part.VERB)) {
+                return false
+            }
+            return checkSentence(part, sex, tail.drop(1))
+        }
+
+        return checkSentence(firstPart, firstSex, words.drop(1))
+    }
+}
+
+fun toSentence(str: String): List<String> = str.split(' ')
+
+fun solveTheProblem(str: String): Boolean {
+    val sentence = toSentence(str)
+    return PetrovskiyLanguage.isValidSentence(sentence)
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val result = if (solveTheProblem(readLine()!!)) "YES" else "NO"
+    print(result)
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,23 @@
 package ru.spbau.mit
-import kotlin.test.assertEquals
+
+import kotlin.test.assertTrue
+
 import org.junit.Test
+import kotlin.test.assertFalse
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testPetr() {
+        assertTrue(solveTheProblem("petr"))
+    }
+
+    @Test
+    fun testBad() {
+        assertFalse(solveTheProblem("etis atis animatis etis atis amatis"))
+    }
+
+    @Test
+    fun testGood() {
+        assertTrue(solveTheProblem("nataliala kataliala vetra feinites"))
     }
 }
